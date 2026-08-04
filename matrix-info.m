1169d1e3233b1ec81de7a9284901284457e34e46
Shashwati Pingalkar <pingalkarshashwati@gmail.com>
2026-08-05 00:17:04 +0530

    Add matrix size, length, and element count calculations

---
+++ matrix-info.m
@@ -0,0 +1,16 @@
+% Given the matrix:
+
+A = [5 10 15;
+     20 25 30];
+
+% Task:
+
+% Find the size of the matrix and store it in S.
+% Find the length of the matrix and store it in L.
+% Find the total number of elements and store it in N.
+
+S = size(A)
+
+L = length(A)
+
+N = numel(A)
