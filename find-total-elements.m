f6dce4b4d712964995f96190c1b9e47b8b868f3a
Shashwati Pingalkar <pingalkarshashwati@gmail.com>
2026-08-05 00:12:54 +0530

    Add function to find total elements in matrix A
    Calculate the total number of elements in matrix A.

---
+++ find-total-elements.m
@@ -0,0 +1,12 @@
+% Given the matrix:
+
+A = [2 4 6;
+     8 10 12;
+     14 16 18;
+     20 22 24];
+
+% Task:
+
+% Find the total number of elements in the matrix and store the result in a variable named N.
+
+N = numel(A)
