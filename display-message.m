014354bdaa1741e6013865ee738ef9c2c579e428
Shashwati Pingalkar <pingalkarshashwati@gmail.com>
2026-08-05 00:14:38 +0530

    Create display-message.m

---
+++ display-message.m
@@ -0,0 +1,8 @@
+% Write a MATLAB program that:
+
+% Creates a variable named course with the value "MATLAB Programming".
+% Displays the value of course using the disp() function.
+
+course = "MATLAB Programming";
+
+disp(course)
