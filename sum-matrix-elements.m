5044e18a1e2bb67e1aed6382f301f7e2288e11cf
Shashwati Pingalkar <pingalkarshashwati@gmail.com>
2026-08-04 23:38:30 +0530

    Implement matrix element summation
    Calculate the sum of all elements in a matrix.

---
+++ sum-matrix-elements.m
@@ -0,0 +1,13 @@
+% Given:
+
+A = [1 2 3;
+     4 5 6;
+     7 8 9];
+
+% Task:
+
+% Find the sum of all elements in the matrix and store the result in a variable named S.
+
+B = sum(A);
+
+S = sum(B)
