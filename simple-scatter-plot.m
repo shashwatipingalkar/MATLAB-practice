90b19ff6df5fa4a6f6e2dd555e9fbdb8132ae494
Shashwati Pingalkar <pingalkarshashwati@gmail.com>
2026-08-07 11:28:29 +0530

    Create simple-scatter-plot.m

---
+++ simple-scatter-plot.m
@@ -0,0 +1,21 @@
+% Create the following vectors:
+
+x = [1 2 3 4 5 6];
+y = [3 5 2 8 7 9];
+
+% Write a MATLAB program that:
+
+% Creates a scatter plot of y versus x.
+% Adds the title "Scatter Plot Example".
+% Labels the x-axis as "X Values".
+% Labels the y-axis as "Y Values".
+% Turns the grid on.
+
+scatter(x,y)
+
+title("Scatter Plot Example")
+
+xlabel("X Values")
+ylabel("Y Values")
+
+grid on
