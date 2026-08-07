0b63fca7beccbc01c31d48c2e69fcf6cc5998ffc
Shashwati Pingalkar <pingalkarshashwati@gmail.com>
2026-08-07 10:57:54 +0530

    Add simple line plot in MATLAB

---
+++ simple-line-plot.m
@@ -0,0 +1,18 @@
+% Create two vectors:
+
+x = [1 2 3 4 5];
+y = [2 4 6 8 10];
+
+% Write a MATLAB program that:
+
+% Plots y versus x.
+% Gives the graph the title "Simple Line Plot".
+% Labels the x-axis as "X Values".
+% Labels the y-axis as "Y Values".
+% Turns the grid on.
+
+plot(x,y)
+title("Simple Line Plot")
+xlabel("X Values")
+ylabel("Y Values")
+grid on
