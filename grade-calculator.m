90a619208605f1dc409800d5036d9b6cc2740bed
Shashwati Pingalkar <pingalkarshashwati@gmail.com>
2026-08-07 12:00:43 +0530

    Implement grade calculation in MATLAB

---
+++ grade-calculator.m
@@ -0,0 +1,27 @@
+% Create a variable:
+
+marks = 78;
+
+% Write a MATLAB program that prints:
+
+% "Grade A" if marks >= 90
+% "Grade B" if marks >= 75
+% "Grade C" if marks >= 60
+% "Grade D" if marks >= 40
+% "Fail" otherwise
+
+% Rules:
+
+% Use one if-elseif-else structure.
+% Use disp() to display the grade.
+
+if marks >= 90
+    disp("Grade A")
+elseif marks >= 75
+    disp("Grade B")
+elseif marks >= 60
+    disp("Grade C")
+elseif marks >=40
+    disp("Grade D")
+else 
+    disp("Fail")
