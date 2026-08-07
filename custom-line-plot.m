9a96e8edcd59e3a611c0a08ae1b59120063bee9f
Shashwati Pingalkar <pingalkarshashwati@gmail.com>
2026-08-07 11:03:23 +0530

    Add MATLAB script for plotting quadratic function

---
+++ custom-line-plot.m
@@ -0,0 +1,23 @@
+% Create the following vectors:
+
+ x = [0 1 2 3 4 5];
+ y = [0 1 4 9 16 25];
+
+% Write a MATLAB program that:
+
+% Plots y versus x.
+% Uses a red solid line with circle markers.
+% Adds the title "Quadratic Function".
+% Labels the x-axis as "Input".
+% Labels the y-axis as "Output".
+% Turns the grid on.
+
+plot(x,y,"r-o")
+
+title("Quadratic Function")
+
+xlabel("Input")
+
+ylabel("Output")
+
+grid on
